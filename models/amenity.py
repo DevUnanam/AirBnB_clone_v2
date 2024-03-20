cdbcc2235441b8e26522d450bdf2857f0223aa26
Unanam Victoria <riaahh20@yahoo.com>
2024-03-20 20:00:04 +0000

    documentation

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This defines the amenity class"""
+"""Here, the amentiy class is defined"""
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
