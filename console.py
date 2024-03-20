2de63b849183c0e60400b4a194a67e9e3d318360
muss001998 <mustapha001hamiai@gmail.com>
2024-03-20 06:04:42 -0700

    task 3 and 4

--- console.py
+++ console.py
@@ -115,7 +115,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, line):
         """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
-        Create a new class instance with given keys/values and print its id.
+        This creates a new class instance with given keys/values and print its id.
         """
         try:
             if not line:
